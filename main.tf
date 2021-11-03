974ad2adbbc20338258af15033e6aaaf91faafbc
amaurybsouza <amaurybsouza@gmail.com>
2021-11-02 22:15:25 -0300

    new file

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# variables used to connect the token
+variable "do_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+    token = var.do_token
+}
+
+# Configure the resource for DigitalOcean
+resource "digitalocean_droplet" "web" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-1"
+  region = "nyc1"
+  size   = "s-1vcpu-1gb"
+}
